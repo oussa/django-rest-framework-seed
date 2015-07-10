b88eed634da773ff33d73ddff05ed0cd4edb0ffc
Oussama <krifa.oussama@gmail.com>
2015-07-10 19:06:05 +0100

    Created a new sample app for ideas

--- app/models.py
+++ app/models.py
@@ -9,7 +9,3 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
-
-
-# Create your models here..
-
